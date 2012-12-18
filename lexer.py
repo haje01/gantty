45a735cd0991b6bc249c9d8d3d1b5c0db2faf08b
haje01 <haje01@gmail.com>
2012-12-19 01:35:51 +0900

    renew lexer, parser

---
+++ lexer.py
@@ -0,0 +1,59 @@
+
+from ply import lex
+
+states = (
+    ('task', 'exclusive'),
+)
+
+tokens = (
+    'TASKNAME',
+    'NEWLINE',
+    'YEARMONTHDAY',
+    'MONTHDAY',
+    'TERM',
+    'PERCENT',
+    'OWNER',
+    'COMMENT',
+)
+
+t_task_YEARMONTHDAY = r'\d{4}/\d{2}/\d{2}'
+t_task_MONTHDAY = r'\d{4}/\d{2}/\d{2}'
+t_task_TERM = r'\d+[hdw]'
+t_task_PERCENT = r'1?\d{1,2}%'
+t_task_OWNER = r'@[\S]+'
+t_task_ignore = ' \t'
+
+def t_COMMENT(t):
+    r'\#.*'
+    t.lexer.push_state('task')
+    return t
+
+def t_task_COMMENT(t):
+    r'\#.*'
+
+def t_TASKNAME(t):
+    r'\s*\[[^\]]+\]'
+    t.lexer.push_state('task')
+    return t
+
+def t_task_NEWLINE(t):
+    r'\n'
+    t.lexer.lineno += 1
+    t.lexer.pop_state()
+    return t
+
+def t_NEWLINE(t):
+    r'\n'
+    t.lexer.lineno += 1
+    return t
+
+def t_error(t):
+    print 'Illegal character ', t.value[0]
+    t.lexer.skip(1)
+
+def t_task_error(t):
+    print 'Illegal task character ', t.value[0]
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
--- parser.py
+++ parser.py
@@ -1,203 +1,114 @@
 #!/usr/bin/env python
-import ply.lex as lex
 import ply.yacc as yacc
+import lexer
 
-tokens = (
-    'TASKNAME',
-    'MILESTONENAME',
-    'YEARMONTHDAY',
-    'MONTHDAY',
-    'SPACE',
-    'TERM',
-    'NEWLINE',
-    'PERCENT',
-    'PERSON',
-    'ARROW',
-)
-
-t_SPACE = r'\s+'
-t_ARROW = r'<-'
-t_ignore_COMMENT = '\#.*'
-
-tasks = {}
-cur_task = None
-
-class Task(object):
-    def __init__(self, name):
-        object.__init__(self)
-        self.name = name
-
-
-def t_TASKNAME(t):
-    r'\[[^\]]+\]'
-    t.value = t.value.strip('[').rstrip(']').strip()
-    return t
-
-def t_MILESTONENAME(t):
-    r'\*[^\]]+\*'
-    t.value = t.value.strip('*').rstrip('*').strip()
-    return t
-
-def t_YEARMONTHDAY(t):
-    r'\d{4}/\d{2}/\d{2}'
-    return t
-
-def t_MONTHDAY(t):
-    r'\d{4}/\d{2}/\d{2}'
-    return t
-
-def t_TERM(t):
-    r'\d[wdh]'
-    return t
-
-def t_PERCENT(t):
-    r'1?\d{1,2}%'
-    return t
-
-def t_PERSON(t):
-    r'@\S+'
-    return t
-
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
-    t.type = 'NEWLINE'
-    return t
-
-def t_error(t):
-    print "Illegal character '%s'" % t.value[0]
-    t.lexer.skip(1)
-
-lexer = lex.lex()
+tokens = lexer.tokens
 
 def p_program(p):
     '''program : program statement
                | statement'''
+    print 'program ', p[1:]
+
+### all statements
 
 def p_statement(p):
-    '''statement : indent task
-                 | indent milestone
-                 | SPACE NEWLINE
-                 | NEWLINE
-                 | SPACE'''
-    p[0] = p[1]
-    print 'statement ', p[0]
+    '''statement : command NEWLINE'''
+    if isinstance(p[1], str):
+        print 'Error ', p[1]
+    else:
+        p[0] = p[1]
+        print 'statement ', p[1:]
 
-def p_indent(p):
-    '''indent : SPACE
+def p_statement_error(p):
+    '''statement : error NEWLINE'''
+    print "Malformed statement at line %d '%s'" % (p[1].lineno, str(p[1].value))
+    p[0] = None
+    p.parser.error = 1
+
+def p_statement_blank(p):
+    '''statement : NEWLINE'''
+
+### comment
+def p_command_comment(p):
+    '''command : COMMENT'''
+    print 'command comment ', p[1]
+
+### task
+def p_command_task(p):
+    '''command : TASKNAME optstart optend optdone optownerlist optcomment'''
+    print 'command task ', p[1:]
+
+### optional start
+def p_optstart(p):
+    '''optstart : date
+                | empty'''
+    print 'optstart ', p[1]
+
+### optional end
+def p_optend(p):
+    '''optend : date
               | empty'''
-    print 'indent ', p[1:]
+    print 'optend ', p[1]
 
-def p_task(p):
-    '''task : TASKNAME NEWLINE
-            | TASKNAME SPACE taskbody NEWLINE'''
-    print 'task ', p[1:]
+### optional done
+def p_optdone(p):
+    '''optdone : PERCENT
+               | empty'''
+    print 'optdone ', p[1]
 
-def p_milestone(p):
-    '''milestone : MILESTONENAME newline
-            | MILESTONENAME SPACE milestonebody newline'''
-    print 'milestone ', p[1:]
+### optional ownerlist
+def p_optownerlist(p):
+    '''optownerlist : ownerlist
+                    | empty'''
+    print 'optownerlist ', p[1]
 
-def p_newline(p):
-    '''newline : NEWLINE
-               | SPACE NEWLINE'''
+### optional comment
+def p_optcomment(p):
+    '''optcomment : COMMENT
+                  | empty'''
+    print 'optcomment ', p[1]
 
-def p_taskbody(p):
-    '''taskbody : period done person depend'''
-    print 'taskbody ', p[0]
+### ownerlist
+def p_ownerlist(p):
+    '''ownerlist : ownerlist OWNER
+                 | OWNER'''
+    print 'ownerlist ', p[1:]
 
-def p_milestonebody(p):
-    '''milestonebody : start space depend'''
-    print 'taskbody ', p[0]
-
-def p_space(p):
-    '''space : SPACE
-             | empty'''
-
-def p_period(p):
-    '''period : start 
-              | start SPACE end
-              | end
-              | empty'''
-
-def p_start(p):
-    '''start : SPACE YEARMONTHDAY
-             | SPACE MONTHDAY'''
+### date
+def p_date(p):
+    '''date : YEARMONTHDAY
+            | MONTHDAY
+            | TERM'''
     p[0] = p[1]
-    print 'start ', p[0]
+    print 'date ', p[1]
 
-def p_end(p):
-    '''end : YEARMONTHDAY
-          | MONTHDAY
-          | TERM'''
-    p[0] = p[1]
-    print 'end ', p[0]
-
-def p_done(p):
-    '''done : SPACE PERCENT
-            | empty'''
-    p[0] = p[1]
-    print 'done ', p[0]
-
-def p_person(p):
-    '''person : person SPACE PERSON
-              | SPACE PERSON
-              | empty'''
-    print 'person ', p[1:]
-
-def p_depend(p):
-    '''depend : ARROW SPACE TASKNAME
-              | ARROW SPACE MILESTONENAME
-              | empty'''
-    print 'depend ', p[1:]
+### empty
 
 def p_empty(p):
-    'empty :'
-    pass
+    '''empty : '''
 
+###  catastrophic error handler
 def p_error(p):
-    if p:
-        print p
-        print "Syntax error at '%s'" % p.value
-    else:
-        print "Ambiguous syntax error"
-
+    if not p:
+        print "Syntax error at EOF"
 
 parser = yacc.yacc()
 
-# Test
+def parse(data):
+    parser.error = 0
+    p = parser.parse(data + '\n')
+    if parser.error:
+        return None
+    return p
+
 def test():
     data = '''
-# comment 1
-*start*
+# line comment
 [task 1]
-[task 2] 2012/01/01
-[task 3] 2012/01/01 2012/02/01 # comment 2
-[task 4] 2012/01/01 2w
-[task 5] 2012/01/01 2w 30%
-[task 6] 2012/01/01 2w 100% @haje01
-[task 7] 2012/01/01 2w 30% @haje01 @lqez
-[task 8] 2012/01/01 2w 30% @haje01 @lqez <- [task 1]
-[task 9]
-    [task 9-1] 2w 0%
-    [task 9-2] 2012/02/15 10% <- [task 9-1]
-        [task 9-2-1] 2012/02/17 3d 0% @haje01
-*end* 2012/03/01 <- [task 9] # comment 3
-    '''
-
-    def lex_output():
-        print 'Tokenizing..'
-        lexer.input(data)
-        while True:
-            tok = lexer.token()
-            if not tok: break
-            print tok.type, tok.value, tok.lineno, tok.lexpos
-    lex_output()
-
-    def parse():
-        print 'Parsing..'
-        parser.parse(data + '\n')
-    parse()
+[task 2] 2012/01/01 @haje01 @lqez # tail comment
+[task 3] 2012/01/01 2w 10% @lqez
+'''
+    print parse(data)
 
 if __name__ == '__main__':
     test()
